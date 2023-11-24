9569f1fc8c92395deb74a75dc6001aa98729ae07
Andrea9808 <andrea.manigrasso98@gmail.com>
2023-11-24 14:45:49 +0100

    feat :bottone + input

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@
 //  gli elementi della lista individualmente con un ciclo while.
 
 
+
+//inizializzo il bottone
+const inputSend = document.querySelector(".send");
+
+//inizializzo 'input
+const inputIngredienti = document.querySelector(".ingredienti");
+
 //selezioniamo ul dell'html
 const listaUl = document.querySelector(".listaspesa");
 
@@ -9,20 +16,37 @@ const listaUl = document.querySelector(".listaspesa");
 const listaSpesa = ["carne","uova","pomodori","mandarini"];
 
 //scorro la lista della spesa con un ciclo while
+//al click del bottone
+inputSend.addEventListener("click", function(){
+
+    const ingrediente = inputIngredienti.value;
+   
+    // aggiunfiamo il nuovo ingrediente alla lista
+    listaSpesa.push(ingrediente);
+
+    // creiamo un nuovo elemento li per il nuovo ingrediente
+    const elementLi = document.createElement("li");
+    elementLi.innerHTML = ingrediente;
+
+    // inseriamo il li creato dentro l'ul
+    listaUl.append(elementLi);
+
+})
+
 
 let i = 0;
 while(i < listaSpesa.length){
 
-    let itemSpesa = listaSpesa[i];
+let itemSpesa = listaSpesa[i];
 
-    //creiamo li che andranno all'interno dell'ul
-    const elementLi = document.createElement("li");
+//creiamo li che andranno all'interno dell'ul
+const elementLi = document.createElement("li");
 
-    //inseriamo dentro il li il contenuto
-    elementLi.append(itemSpesa);
+//inseriamo dentro il li il contenuto
+elementLi.append(itemSpesa);
 
-    //inseriamo il li creato dentro l'ul
-    listaUl.append(elementLi);
+//inseriamo il li creato dentro l'ul
+listaUl.append(elementLi);
 
-    i++;
+i++;
 }
